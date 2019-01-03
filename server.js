7fd2d5d60666445914595da1235d9bd639338f22
Nitesh Ranjan <>
2019-01-03 14:13:58 +0530

    Added mysql database to take name and score through terminal.

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const http = require('http');
+const connection = require('./connection').connection;
 
 const app = express();
 
-app.get('/',(req,res) => {
-  res.send('whats up ?');
+app.get('/put/:name/:score',(req,res) => {
+  let name = req.params.name;
+  let score = req.params.score;
+  let values = [[name, score]];
+
+  connection.query('INSERT INTO developers (name, score) VALUES ?', [values], function (err) {
+    if(err) {
+      res.send('Error saving your information');
+    }
+  });
+
+  res.send('Successfully saved your information');
+})
+
+
+app.get('/get/:name',(req,res) => {
+  let name = req.params.name;
+
+  connection.query("SELECT * FROM developers WHERE name=?", [name], function (err, result) {
+    res.send(result);
+  });
+
+  // res.send('whats up');
 })
 
 const server = http.createServer(app);
 
-server.listen(443, ()=> console.log('Server is listening at port 443'));
+server.listen(8000, ()=> console.log('Server is listening at port 8000'));
